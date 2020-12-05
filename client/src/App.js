165b03adac3eb13d35946d4fff15b49f0346474b
tejaattainu <tejspvg@gmail.com>
2020-12-06 00:23:40 +0530

    project is completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import Results from './components/Results';
 class App extends Component {
 	state = {
 		rollNumbers: '',
-		results: []
+		results: [],
+		disable: false
 	};
 
 	handleChange = (e) => {
@@ -20,6 +21,7 @@ class App extends Component {
 			const { data } = await axios.post('https://eduteclog.herokuapp.com/getresults', data1, { headers: headers });
 			this.setState({ results: data.results });
 		}
+		this.setState({ disable: true})
 	};
 
 	render() {
@@ -27,7 +29,7 @@ class App extends Component {
 			<div>
 				<center>
 					<input type="text" name="rollNumbers" onChange={this.handleChange} />
-					<input type="button" value="submit" onClick={this.handleSubmit} />
+					<button onClick={this.handleSubmit} disabled={this.state.disable}> Submit</button>
 					{this.state.results.length === 0 ? <h1>Fill the input , click on submit and wait</h1> : <Results results={this.state.results} />}
 				</center>
 			</div>
