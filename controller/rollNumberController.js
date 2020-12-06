03188dc187081c461932cf49c054e3a70e664955
tejaattainu <tejspvg@gmail.com>
2020-12-06 09:15:28 +0530

    project is completed

--- controller/rollNumberController.js
+++ controller/rollNumberController.js
@@ -5,16 +5,25 @@ module.exports = {
 		const rollNums = req.body;
 		try {
 			let results = [];
+			let urls = [];
+
 			for (let i = 0; i < rollNums.length; i++) {
-				const data = await fetch(`http://proedge.me/test.php?rollnumber=${Number(rollNums[i])}`);
-				const data1 = await data.text();
-				results.push({
-					rollNumber: rollNums[i],
-					result: data1
-				});
+				urls.push(`http://proedge.me/test.php?rollnumber=${Number(rollNums[i])}`);
 			}
-			res.status(200).json({ results });
+
+			Promise.all(urls.map((u) => fetch(u)))
+				.then((res) => Promise.all(res.map((res) => res.text())))
+				.then((texts) => {
+					for (let i = 0; i < rollNums.length; i++) {
+						results.push({
+							rollNumber: rollNums[i],
+							result: texts[i]
+						});
+					}
+					res.status(200).json({ results });
+				});
 		} catch (err) {
+			//console.log(err);
 			res.status(400).json({ err: err.message });
 		}
 	}
